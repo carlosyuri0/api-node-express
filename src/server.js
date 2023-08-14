82e7f6a63b5755926379702f352034be8b6f515a
carlos <carlosyurialvesbenedito@gmail.com>
2023-08-14 16:53:25 -0300

    rotas

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
-const express = require('express')
+// const express = require('express')
+
+import express from 'express'
+import userRouter from './routes/userRouter.js'
+import productRouter from './routes/productRouter.js'
 
 const api = express()
 
@@ -6,6 +10,9 @@ api.get('/', (req, res) => {
     res.json({message: "Bem-vindo a nossa API"})
 })
 
+api.use('/user', userRouter)
+api.use('/product', productRouter)
+
 api.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000. http://localhost:3000')
+    console.log('Servidor rodando na porta 3000! http://localhost:3000')
 })
